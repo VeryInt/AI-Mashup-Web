eb277e9b15d627b2909b184291fef9a94ad13cb4
loo-y <loo-y@outlook.com>
2024-05-23 12:10:10 +0000

    feat: create store

---
+++ app/(pages)/main/page.tsx
@@ -0,0 +1,35 @@
+'use client'
+import { useEffect } from 'react'
+import { useMainStore } from './providers'
+import { MainStoreProvider } from './providers'
+const Main = () => {
+    const { isloading, updateIsLoading } = useMainStore(state => state)
+    useEffect(() => {
+        let vh = window.innerHeight * 0.01
+        // Then we set the value in the --vh custom property to the root of the document
+        document.documentElement.style.setProperty('--vh', `${vh}px`)
+
+        // We listen to the resize event
+        window.addEventListener('resize', () => {
+            // We execute the same script as before
+            let vh = Math.min(document?.documentElement?.clientHeight || window.innerHeight, window.innerHeight) * 0.01
+            console.log(`resizing, new view height`, vh)
+            document.documentElement.style.setProperty('--vh', `${vh}px`)
+        })
+    }, [])
+
+    return (
+        <div className="flex flex-col w-full items-center">
+        </div>
+    )
+}
+
+const MainPage = () => {
+    return (
+        <MainStoreProvider>
+            <Main></Main>
+        </MainStoreProvider>
+    )
+}
+
+export default MainPage
